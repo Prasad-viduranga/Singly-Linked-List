5a891c2f2a59d2fd65cf81a8b40dd9b739433b1e
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-09 23:03:07 +0530

    Creats objects for store data

--- src/NodeTest.java
+++ src/NodeTest.java
@@ -2,9 +2,12 @@ import ds.Node;
 
 public class NodeTest {
     public static void main(String[] args) {
-        Node node=new Node();
+        Node node1 = new Node(10);
+        Node node2 = new Node(15);
+        Node node3 = new Node(20);
+        Node node4 = new Node(25);
+        Node node5 = new Node(30);
 
-        node.add(1);
 
     }
 
--- src/ds/Node.java
+++ src/ds/Node.java
@@ -4,39 +4,41 @@ public class Node {
     private int data;
     private int next;
 
-    public void add(int num){
-
-
-    }
-
-    public void add(int index,int num){
+    public void add(int num) {
+        data = num;
+        next = 0;
 
     }
 
-    public void remove(int index){
+    public void add(int index, int num) {
 
     }
 
-    public int get(int index){
+    public void remove(int index) {
+
+    }
+
+    public int get(int index) {
         return 0;
     }
 
-    public void print(){
+    public void print() {
 
     }
 
-    public void clear(){
+    public void clear() {
 
     }
 
-    public int size(){
+    public int size() {
         return 0;
     }
 
-    public boolean contains(int num){
+    public boolean contains(int num) {
         return true;
     }
-    public boolean empty(){
+
+    public boolean empty() {
         return true;
     }
 
