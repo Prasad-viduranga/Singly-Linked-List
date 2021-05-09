6ea8f78d6b23d1d8de74506dff6bd83678cfecf5
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-09 22:53:04 +0530

    Create methods

---
+++ src/NodeTest.java
@@ -0,0 +1,11 @@
+import ds.Node;
+
+public class NodeTest {
+    public static void main(String[] args) {
+        Node node=new Node();
+
+        node.add(1);
+
+    }
+
+}
---
+++ src/ds/Node.java
@@ -0,0 +1,43 @@
+package ds;
+
+public class Node {
+    private int data;
+    private int next;
+
+    public void add(int num){
+
+
+    }
+
+    public void add(int index,int num){
+
+    }
+
+    public void remove(int index){
+
+    }
+
+    public int get(int index){
+        return 0;
+    }
+
+    public void print(){
+
+    }
+
+    public void clear(){
+
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public boolean contains(int num){
+        return true;
+    }
+    public boolean empty(){
+        return true;
+    }
+
+}
